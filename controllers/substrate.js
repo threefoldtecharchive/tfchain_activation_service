feccd9823d22e97aaaecb8eef1152cbbc106d9db
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-06-23 16:07:48 +0200

    add whitelist functionality

--- controllers/substrate.js
+++ controllers/substrate.js
@@ -1,6 +1,7 @@
 const { client } = require('../lib/substrate')
 const httpError = require('http-errors')
 const { take } = require('lodash')
+const whitelist = require('../whitelist.json')
 
 // 500 tokens with 12 decimals precision
 const AMOUNT = 1e12 * 500
@@ -9,6 +10,16 @@ const { KYC_PUBLIC_KEY } = process.env
 
 async function validateActivation (body) {
   const { kycSignature, data, substrateAccountID } = body
+
+  // allow whitelisted users to be funded whenever they want
+  if (whitelist.includes(substrateAccountID)) {
+    try {
+      await client.transfer(substrateAccountID, AMOUNT)
+    } catch (error) {
+      throw httpError(error)
+    }
+    return
+  }
 
   const { email, name: identifier } = data
   const originalData = `{ "email": "${email}", "identifier": "${identifier}" }`
